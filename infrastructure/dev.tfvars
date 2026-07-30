3fffb75d4757dbaf0e8b1a1b346280f9759628ee
BenjaminFlan <Benjamin.Flanagan@kainos.com>
2026-07-30 15:17:30 +0100

    remove frontend container app until image is available

--- infrastructure/dev.tfvars
+++ infrastructure/dev.tfvars
@@ -3,7 +3,7 @@ environment = "dev"
 acr_name           = "acraiacademy26"
 acr_resource_group = "rg-ai-academy-26"
 backend_image_tag  = "main"
-frontend_image_tag = "main"
+frontend_image_tag = "9b5d5904-prod-deps-2026-07-30-11-06-05"
 
 aws_region     = "eu-west-1"
 s3_bucket_name = "REPLACE_WITH_YOUR_S3_BUCKET_NAME"
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,11 +3,6 @@ output "resource_group_name" {
   value       = module.resource_group.name
 }
 
-output "frontend_url" {
-  description = "The public URL of the frontend container app."
-  value       = "https://${azurerm_container_app.frontend.ingress[0].fqdn}"
-}
-
 output "backend_fqdn" {
   description = "The internal FQDN of the backend container app (not publicly accessible)."
   value       = azurerm_container_app.backend.ingress[0].fqdn
